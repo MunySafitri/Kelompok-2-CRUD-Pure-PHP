af13faea994d397847f14ffc0dace2de53249f1c
Naufal Alkhalis <94673383+naufalalkhalls@users.noreply.github.com>
2022-04-21 00:20:29 +0700

    css halaman index
    kalo mau pake header footenya panggil aja css yang ini ke halaman add + edit data

--- index.php
+++ index.php
@@ -9,26 +9,43 @@ if( isset($_POST["cari"]) ) {
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Halaman Admin</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Munyet</title>
+    <link rel="stylesheet" href="style.css">
+  
+   
 </head>
 <body>
 
-<h1>Daftar Mahasiswa</h1>
 
-<a href="tambah.php">Tambah data mahasiswa</a>
+<nav>
+	<div class="wrapper">
+		<div class="logo"><a href="">Munyet</a></div>
+		<div class="menu">
+			<ul>
+				<li><a href="/Home">Home</a></li>
+				<li><a href="/Contact">Contact</a></li> 
+				<li><a href="/About">About</a></li>
+				<li><a href="/Help">Help</a></li>
+				<li><a href="" class="tbl-hijau">Log out</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<h1>Daftar Mahasiswa</h1>
 <br><br>
 
 <form action="" method="post">
-
-	<input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian.." autocomplete="off">
-	<button type="submit" name="cari">Cari!</button>
-	
+	<a class="tbh" href="tambah.php">Tambah</a>
+	<br><br>
 </form>
-
 <br>
-<table border="1" cellpadding="10" cellspacing="0">
+<table class="table1" border="1" cellpadding="10" cellspacing="0">
 
 	<tr>
 		<th>No.</th>
@@ -45,10 +62,10 @@ if( isset($_POST["cari"]) ) {
 	<tr>
 		<td><?= $i; ?></td>
 		<td>
-			<a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> |
-			<a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin?');">hapus</a>
+			<a href="ubah.php?id=<?= $row["id"]; ?>" class="ubah">ubah</a> |
+			<a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin?');" class="hapus">hapus</a>
 		</td>
-		<td><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
+		<td><img src="img/<?= $row["gambar"]; ?>" width="100px" height="90px"></td>
 		<td><?= $row["nrp"]; ?></td>
 		<td><?= $row["nama"]; ?></td>
 		<td><?= $row["email"]; ?></td>
@@ -58,6 +75,35 @@ if( isset($_POST["cari"]) ) {
 	<?php endforeach; ?>
 	
 </table>
+    <div id="contact">
+        <div class="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>CoUrgent</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, officia?</p>
+                </div>
+                <div class="footer-section">
+                    <h3>About</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, officia?</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Contact</h3>
+                    <p>Jl. Sultan iskandar muda, Aceh besar,</p>
+                    <p>Kode Pos: 1234</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Sosial</h3>
+                    <p><b>YouTube: </b> Munyet</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="copyright">
+        <div class="wrapper">
+            &copy; 2022. <b>Munyet</b> All Right Reserve
+        </div>
+    </div>
 
 </body>
 </html>
