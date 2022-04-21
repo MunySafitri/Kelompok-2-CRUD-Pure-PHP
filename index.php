45b1a36d8bbdd668c51659649549ad5e9750bf9f
razanfawwaz <razanfawwaz1905@gmail.com>
2022-04-21 10:13:51 +0700

    add lign

--- index.php
+++ index.php
@@ -1,21 +1,37 @@
 <?php 
 require 'functions.php';
-$mahasiswa = query("SELECT * FROM mahasiswa");
+session_start();
+
+//algoritma untuk pagination
+$jumlahDataPerhalaman = 4;
+$jumlahData = count(query("SELECT * FROM mahasiswa"));//jumlah datanya
+$jumlahHalaman = round($jumlahData / $jumlahDataPerhalaman);
+
+//cek jika ada input laman di url
+$halamanAktif = (isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
+//batas
+$awalData = ($jumlahDataPerhalaman * $halamanAktif) - $jumlahDataPerhalaman;
+
+
+
+
+$mahasiswa = query("SELECT * FROM mahasiswa LIMIT $awalData ,$jumlahDataPerhalaman");
 
 // tombol cari ditekan
 if( isset($_POST["cari"]) ) {
 	$mahasiswa = cari($_POST["keyword"]);
 }
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Munyet</title>
+    <title>Data Mahasiswa</title>
     <link rel="stylesheet" href="style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   
    
 </head>
@@ -24,86 +40,127 @@ if( isset($_POST["cari"]) ) {
 
 <nav>
 	<div class="wrapper">
-		<div class="logo"><a href="">Munyet</a></div>
+		<div class="logo"><a href="">Data Mahasiswa</a></div>
 		<div class="menu">
 			<ul>
-				<li><a href="/Home">Home</a></li>
-				<li><a href="/Contact">Contact</a></li> 
-				<li><a href="/About">About</a></li>
-				<li><a href="/Help">Help</a></li>
-				<li><a href="" class="tbl-hijau">Log out</a></li>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="#Contact">Contact</a></li> 
+				<li><a href="#About">About</a></li>
+				<li><a href="#Sosial">Help</a></li>
+				<?php 
+				if($_SESSION["loggedIn"] == true){
+				echo "<li><a href=\"./logout.php\" class=\"tbl-hijau\">Logout</a></li>";
+				}
+				else{
+				echo "<li><a href=\"./login.php\" class=\"tbl-hijau\">Login</a></li>";
+				} ?>
 			</ul>
 		</div>
 	</div>
 </nav>
 
 <h1>Daftar Mahasiswa</h1>
-<br><br>
-
-<form action="" method="post">
-	<a class="tbh" href="tambah.php">Tambah</a>
-	<br><br>
-</form>
 <br>
-<table class="table1" border="1" cellpadding="10" cellspacing="0">
 
-	<tr>
-		<th>No.</th>
-		<th>Aksi</th>
-		<th>Gambar</th>
-		<th>NRP</th>
-		<th>Nama</th>
-		<th>Email</th>
-		<th>Jurusan</th>
-	</tr>
+<div class="container">
+	<form action="" method="post" class="nosubmit position-relative">
 
-	<?php $i = 1; ?>
-	<?php foreach( $mahasiswa as $row ) : ?>
-	<tr>
-		<td><?= $i; ?></td>
-		<td>
-			<a href="ubah.php?id=<?= $row["id"]; ?>" class="ubah">ubah</a> |
-			<a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin?');" class="hapus">hapus</a>
-		</td>
-		<td><img src="img/<?= $row["gambar"]; ?>" width="100px" height="90px"></td>
-		<td><?= $row["nrp"]; ?></td>
-		<td><?= $row["nama"]; ?></td>
-		<td><?= $row["email"]; ?></td>
-		<td><?= $row["jurusan"]; ?></td>
-	</tr>
-	<?php $i++; ?>
-	<?php endforeach; ?>
+		<input type="text" name="keyword" size="40" autofocus placeholder="Masukkan keyword pencarian.." autocomplete="off" class="form-control  " style="width:30%; float:left">
+		
+		<button type="submit" name="cari" class="btn btn-teal me-2 me-auto mb-2 " style="background:green;color:white ">Cari!</button>
+		
+		<br><br>
+		<?php 
+		if($_SESSION["loggedIn"] == true){
+			echo "<a class=\"tbl-hijau\" href=\"tambah.php\">Tambah</a>";
+		}
+		else{
+			echo "<a>Login terlebih dahulu untuk menambahkan data!</a>";
+		}
+		?>
+		<br>
+
+	</form>
+	<br>
+
+	<table class="table1" border="1" cellspacing="10" cellpadding="20" style = "margin-top:20px;position:center;">
+
+		<tr>
+			<th>No.</th>
+			<th>Aksi</th>
+			<th>Gambar</th>
+			<th>NRP</th>
+			<th>Nama</th>
+			<th>Email</th>
+			<th>Jurusan</th>
+		</tr>
+
+		<?php $i = 1; ?>
+		
+		<?php foreach( $mahasiswa as $row ) : ?>
+		<tr>
+			<td><?= $i; ?></td>
+			<td>
+				<a href="ubah.php?id=<?= $row["id"]; ?>" class="ubah">ubah</a> |
+				<a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin?');" class="hapus">hapus</a>
+			</td>
+			<td><img src="img/<?= $row["gambar"]; ?>" width="100px" height="120px"></td>
+			<td><?= $row["nrp"]; ?></td>
+			<td><?= $row["nama"]; ?></td>
+			<td><?= $row["email"]; ?></td>
+			<td><?= $row["jurusan"]; ?></td>
+		</tr>
+		<?php $i++; ?>
+		<?php endforeach; ?>
+		
+	</table>
+	<br>
+
+	<?php if($halamanAktif > 1): ?>
+		<a href= "?halaman= <?=$halamanAktif -1;?>"> &lt; </a>
+	<?php endif; ?>
+
 	
-</table>
-    <div id="contact">
+	<!-- navigasi -->
+	<?php for ($i = 1 ; $i <= $jumlahHalaman; $i++) : ?>
+			<?php if($i == $halamanAktif): ?>
+				<a href="?halaman= <?= $i ?>" class="ubah" style="font-weight:bold; color-font:black margin-right:80px; font-size:20px;"> <?= $i ?> </a>
+			<?php else: ?>
+				<a style="font-size:20px;" href="?halaman= <?= $i ?>"> <?= $i ?> </a>
+			<?php endif; ?>
+	<?php endfor;?>
+</div>
+
+    <div id="contact" style="margin-top: 50px;">
         <div class="wrapper">
             <div class="footer">
-                <div class="footer-section">
-                    <h3>CoUrgent</h3>
+                <div class="footer-section" id="CoUrgent">
+                    <h3 style="text-align:center;">CoUrgent</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, officia?</p>
                 </div>
-                <div class="footer-section">
-                    <h3>About</h3>
+                <div class="footer-section" id="About">
+                    <h3 style="text-align:center;">About</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, officia?</p>
                 </div>
-                <div class="footer-section">
-                    <h3>Contact</h3>
+                <div class="footer-section" id="Contact">
+                    <h3 style="text-align:center;">Contact</h3>
                     <p>Jl. Sultan iskandar muda, Aceh besar,</p>
                     <p>Kode Pos: 1234</p>
                 </div>
-                <div class="footer-section">
-                    <h3>Sosial</h3>
-                    <p><b>YouTube: </b> Munyet</p>
+                <div class="footer-section" id="Sosial">
+                    <h3 style="text-align:center;">Sosial</h3>
+                    <p><b>YouTube: </b> Kelompok-2</p>
                 </div>
             </div>
+			<div id="copyright">
+				<div class="wrapper">
+					&copy; 2022. <b>Kelompok-2</b> All Right Reserve
+				</div>
+			</div>
         </div>
     </div>
 
-    <div id="copyright">
-        <div class="wrapper">
-            &copy; 2022. <b>Munyet</b> All Right Reserve
-        </div>
-    </div>
+    
 
 </body>
 </html>
---
+++ registrasi.php
@@ -0,0 +1,32 @@
+<?php 
+require 'functions.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registrasi User</title>
+</head>
+<body>
+	<h3>Registrasi User</h3>
+	<form action="" method="post">
+		<ul>
+			<li>
+				<label for="username">Username :</label>
+				<input type="text" name="username" id="username" required>
+			</li>
+			<li>
+				<label for="password">Password :</label>
+				<input type="password" name="password" id="password" required>
+			</li>
+			<li>
+				<label for="email">Email :</label>
+				<input type="text" name="email" id="email" required>
+			</li>
+			<li>
+				<button type="submit" name="register">Register</button>
+			</li>
+		</ul>
+	</form>
+
